c1e2c8cc8d0fec0063f69424955c830bf040bc3b
Bhargava Raju <BhargavaR@anthology.com>
2021-02-20 18:10:19 +0530

    Added controller logic to validate the user credentials

--- ImageHoster/src/main/java/ImageHoster/controller/UserController.java
+++ ImageHoster/src/main/java/ImageHoster/controller/UserController.java
@@ -40,9 +40,26 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+        String password = user.getPassword();
+        String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+        Boolean isPasswordValid = passwordValidation(password);
+        if (isPasswordValid) {
+            // Successfully return the user to login if the password is strong.
+            userService.registerUser(user);
+            return "users/login";
+        } else {
+            // Display error message to the user on the registration page.
+            user.setUsername(null);
+            model.addAttribute("User", user);
+            model.addAttribute("passwordTypeError", error);
+            return "users/registration";
+
+        }
+    }
+
+    public static boolean passwordValidation(String password) {
+        return false;
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
